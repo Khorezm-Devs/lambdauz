ed2a4bff20d4dd736a45fa1d795cb2e7f544566a
Maftunbek <maftunbek.r@gmail.com>
2020-08-19 17:31:12 +0500

    code style small change

--- app/filters/ExampleFilter.scala
+++ app/filters/ExampleFilter.scala
@@ -5,6 +5,16 @@ import javax.inject._
 import play.api.mvc._
 import scala.concurrent.{ExecutionContext, Future}
 
+/**
+ * This is a simple filter that adds a header to all requests. It's
+ * added to the application's list of filters by the
+ * [[Filters]] class.
+ *
+ * @param mat This object is needed to handle streaming of requests
+ * and responses.
+ * @param exec This class is needed to execute code asynchronously.
+ * It is used below by the `map` method.
+ */
 @Singleton
 class ExampleFilter @Inject()(
     implicit override val mat: Materializer,
